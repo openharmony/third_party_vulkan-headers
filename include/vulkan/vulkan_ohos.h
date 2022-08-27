ab9d59cd9e991044890a6d522d28c53220a327fb
andrewhw <zhangzhao62@huawei.com>
2022-08-27 10:39:32 +0800

    add ohos surface extension
    Signed-off-by: andrewhw <zhangzhao62@huawei.com>

--- include/vulkan/vulkan_ohos.h
+++ include/vulkan/vulkan_ohos.h
@@ -17,28 +17,92 @@
 extern "C" {
 #endif
 
-
-
-#define VK_OHOS_openhm_surface 1
-#define VK_OHOS_OPENHM_SURFACE_SPEC_VERSION 1
-#define VK_OHOS_OPENHM_SURFACE_EXTENSION_NAME "VK_OHOS_openhm_surface"
-typedef VkFlags VkOpenhmSurfaceCreateFlagsOHOS;
-typedef struct VkOpenhmSurfaceCreateInfoOHOS {
+#define VK_OpenHarmony_OHOS_surface 1
+struct NativeWindow;
+#define VK_OPENHARMONY_OHOS_SURFACE_SPEC_VERSION 1
+#define VK_OPENHARMONY_OHOS_SURFACE_EXTENSION_NAME "VK_OpenHarmony_OHOS_surface"
+// VK_STRUCTURE_TYPE_OHOS_SURFACE_CREATE_INFO_OPENHARMONY
+typedef VkFlags VkOHOSSurfaceCreateFlagsOpenHarmony;
+typedef struct VkOHOSSurfaceCreateInfoOpenHarmony {
     VkStructureType                   sType;
     const void*                       pNext;
-    VkOpenhmSurfaceCreateFlagsOHOS    flags;
-    OHNativeWindow*                   window;
-} VkOpenhmSurfaceCreateInfoOHOS;
+    VkOHOSSurfaceCreateFlagsOpenHarmony    flags;
+    NativeWindow*                   window;
+} VkOHOSSurfaceCreateInfoOpenHarmony;
 
-typedef VkResult (VKAPI_PTR *PFN_VkCreateOpenhmSurfaceOHOS)(VkInstance instance, const VkOpenhmSurfaceCreateInfoOHOS* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkSurfaceKHR* pSurface);
+typedef VkResult (VKAPI_PTR *PFN_vkCreateOHOSSurfaceOpenHarmony)(VkInstance instance, const VkOHOSSurfaceCreateInfoOpenHarmony* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkSurfaceKHR* pSurface);
 
 #ifndef VK_NO_PROTOTYPES
-VKAPI_ATTR VkResult VKAPI_CALL VkCreateOpenhmSurfaceOHOS(
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateOHOSSurfaceOpenHarmony(
     VkInstance                                  instance,
-    const VkOpenhmSurfaceCreateInfoOHOS*        pCreateInfo,
+    const VkOHOSSurfaceCreateInfoOpenHarmony*        pCreateInfo,
     const VkAllocationCallbacks*                pAllocator,
     VkSurfaceKHR*                               pSurface);
 #endif
+
+#define VK_OpenHarmony_external_memory_OHOS_native_buffer 1
+struct OH_NativeBuffer;
+#define VK_OPENHARMONY_EXTERNAL_MEMORY_OHOS_NATIVE_BUFFER_SPEC_VERSION 1
+#define VK_OPENHARMONY_EXTERNAL_MEMORY_OHOS_NATIVE_BUFFER_EXTENSION_NAME "VK_OpenHarmony_external_memory_OHOS_native_buffer"
+typedef struct VkOHOSNativeBufferUsageOpenHarmony {
+    VkStructureType    sType;
+    void*              pNext;
+    uint64_t           OHOSNativeBufferUsage;
+} VkOHOSNativeBufferUsageOpenHarmony;
+
+typedef struct VkOHOSNativeBufferPropertiesOpenHarmony {
+    VkStructureType    sType;
+    void*              pNext;
+    VkDeviceSize       allocationSize;
+    uint32_t           memoryTypeBits;
+} VkOHOSNativeBufferPropertiesOpenHarmony;
+
+typedef struct VkOHOSNativeBufferFormatPropertiesOpenHarmony {
+    VkStructureType                  sType;
+    void*                            pNext;
+    VkFormat                         format;
+    uint64_t                         externalFormat;
+    VkFormatFeatureFlags             formatFeatures;
+    VkComponentMapping               samplerYcbcrConversionComponents;
+    VkSamplerYcbcrModelConversion    suggestedYcbcrModel;
+    VkSamplerYcbcrRange              suggestedYcbcrRange;
+    VkChromaLocation                 suggestedXChromaOffset;
+    VkChromaLocation                 suggestedYChromaOffset;
+} VkOHOSNativeBufferFormatPropertiesOpenHarmony;
+
+typedef struct VkImportOHOSNativeBufferInfoOpenHarmony {
+    VkStructureType            sType;
+    const void*                pNext;
+    struct OH_NativeBuffer*    buffer;
+} VkImportOHOSNativeBufferInfoOpenHarmony;
+
+typedef struct VkMemoryGetOHOSNativeBufferInfoOpenHarmony {
+    VkStructureType    sType;
+    const void*        pNext;
+    VkDeviceMemory     memory;
+} VkMemoryGetOHOSNativeBufferInfoOpenHarmony;
+
+typedef struct VkExternalFormatOpenHarmony {
+    VkStructureType    sType;
+    void*              pNext;
+    uint64_t           externalFormat;
+} VkExternalFormatOpenHarmony;
+
+typedef VkResult (VKAPI_PTR *PFN_vkGetOHOSNativeBufferPropertiesOpenHarmony)(VkDevice device, const struct OH_NativeBuffer* buffer, VkOHOSNativeBufferPropertiesOpenHarmony* pProperties);
+typedef VkResult (VKAPI_PTR *PFN_vkGetMemoryOHOSNativeBufferOpenHarmony)(VkDevice device, const VkMemoryGetOHOSNativeBufferInfoOpenHarmony* pInfo, struct OH_NativeBuffer** pBuffer);
+
+#ifndef VK_NO_PROTOTYPES
+VKAPI_ATTR VkResult VKAPI_CALL vkGetOHOSNativeBufferPropertiesOpenHarmony(
+    VkDevice                                    device,
+    const struct OH_NativeBuffer*               buffer,
+    VkOHOSNativeBufferPropertiesOpenHarmony*         pProperties);
+
+VKAPI_ATTR VkResult VKAPI_CALL vkGetMemoryOHOSNativeBufferOpenHarmony(
+    VkDevice                                     device,
+    const VkMemoryGetOHOSNativeBufferInfoOpenHarmony* pInfo,
+    struct OH_NativeBuffer**                     pBuffer);
+#endif
+
 
 #ifdef __cplusplus
 }
