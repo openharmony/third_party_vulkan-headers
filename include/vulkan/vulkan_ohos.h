3bf4ab3a4564908a3525d03694d42d3498fd0ebe
andrew0229 <zhangzhao62@huawei.com>
2025-10-30 11:44:36 +0000

    remove const because uptream
    Signed-off-by: andrew0229 <zhangzhao62@huawei.com>

--- include/vulkan/vulkan_ohos.h
+++ include/vulkan/vulkan_ohos.h
@@ -66,7 +66,7 @@ typedef struct VkSwapchainImageCreateInfoOHOS {
 
 typedef struct VkPhysicalDevicePresentationPropertiesOHOS {
     VkStructureType    sType;
-    const void*        pNext;
+    void*              pNext;
     VkBool32           sharedImage;
 } VkPhysicalDevicePresentationPropertiesOHOS;
 
